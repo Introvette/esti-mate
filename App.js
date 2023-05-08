e6f8c12488a39d0873d88855166f82cc6564782d
Yvette Rosario <vette.rosario@gmail.com>
2023-05-07 23:17:53 -0400

    Login works, need to adjust stack

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import WelcomeScreen from './screens/WelcomeScreen';
+import LoginScreen from './authorization/LogIn';
 
+const Stack = createStackNavigator();
+
+const WelcomeScreen = ({ navigation }) => {
+  const handleLoginPress = () => {
+    navigation.navigate('Login');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button title="Go to Login" onPress={handleLoginPress} />
+    </View>
+  );
+};
 
 const App = () => {
-  return <WelcomeScreen />;
-
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Login' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+});
 
 export default App;
---
+++ authorization/LogIn.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState(false);
+
+  const handleLogin = () => {
+    if (username === 'admin' && password === 'password') {
+      console.log('Login successful');
+    } else {
+      console.log('Login failed');
+      setLoginError(true);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login Screen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+      {loginError && (
+        <Text style={styles.errorText}>Invalid username or password</Text>
+      )}
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 16,
+  },
+});
+
+export default LoginScreen;
